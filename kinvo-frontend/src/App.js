3eb520ad07ddcba9981ff3c9771c4a99499c0ed1
Gabriel Jony <gabjony@gmail.com>
2019-08-07 23:28:22 -0300

    limpeza no código react

--- kinvo-frontend/src/App.js
+++ kinvo-frontend/src/App.js
@@ -1,23 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src={logo} alt="kinvo logo" />
+        <span className="title">premium</span>
       </header>
     </div>
   );
