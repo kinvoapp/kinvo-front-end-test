f5d2b2efb946b5c7fee02934f3d25f016dbf6498
Vitor Emanuel <veslima3@gmail.com>
2019-09-16 17:55:54 -0300

    Montando a lógica de interação com as tabs

--- kinvo/js/actions.js
+++ kinvo/js/actions.js
@@ -1,1 +1,17 @@
 
+let tabButtons = document.querySelectorAll(".tab-container .button-container button");
+let tabPanels = document.querySelectorAll(".tab-container .tab-panel");
+
+function showPanel(panelIndex, colorCode){
+  tabButtons.forEach(function(node){
+    node.style.backgroundColor = "";
+    node.style.color = "";
+  });
+  tabButtons[panelIndex].style.backgroundColor = colorCode;
+    tabButtons[panelIndex].style.color = "blue";
+    tabPanels.forEach(function(node){
+      node.style.display = "none";
+    });
+    tabPanels[panelIndex].style.display = "block";
+    tabPanels[panelIndex].style.backgroundColor = colorCode;
+}
