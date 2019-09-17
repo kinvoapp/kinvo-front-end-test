2fa46930b2b63cda46d4165a47989be5d4ab82dc
Marcos Adriano <mdrick19@gmail.com>
2019-09-17 05:58:06 -0300

    Adiciona component Header

---
+++ desafio-kinvo/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import logo from '../../logo.svg'
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">
+        <img id="logo" src={logo}></img>
+    </header>
+);
+
+export default Header;
