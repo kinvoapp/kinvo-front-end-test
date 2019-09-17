af7f7b55a40961873f7812ee5cc656cf051f0db5
Vitor Emanuel <veslima3@gmail.com>
2019-09-17 00:30:19 -0300

    Update actions.js

--- kinvo/js/actions.js
+++ kinvo/js/actions.js
@@ -1,17 +1,18 @@
 
-let tabButtons = document.querySelectorAll(".tab-container .button-container button");
-let tabPanels = document.querySelectorAll(".tab-container .tab-panel");
+let tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
+let tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
 
-function showPanel(panelIndex, colorCode){
-  tabButtons.forEach(function(node){
-    node.style.backgroundColor = "";
-    node.style.color = "";
-  });
-  tabButtons[panelIndex].style.backgroundColor = colorCode;
-    tabButtons[panelIndex].style.color = "blue";
-    tabPanels.forEach(function(node){
-      node.style.display = "none";
+function showPanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
     });
-    tabPanels[panelIndex].style.display = "block";
-    tabPanels[panelIndex].style.backgroundColor = colorCode;
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabButtons[panelIndex].style.color="darkgray";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor=colorCode;
 }
+showPanel(0,'#f44336');
