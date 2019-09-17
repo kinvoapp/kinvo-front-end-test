f2a7a7437875e430062550cde7b79c66605ba196
Vitor Emanuel <veslima3@gmail.com>
2019-09-17 06:07:50 -0300

    Update actions.js

--- kinvo/js/actions.js
+++ kinvo/js/actions.js
@@ -1,4 +1,4 @@
-
+//tab animations
 let tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
 let tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
 
@@ -8,11 +8,34 @@ function showPanel(panelIndex,colorCode) {
         node.style.color="";
     });
     tabButtons[panelIndex].style.backgroundColor=colorCode;
-    tabButtons[panelIndex].style.color="darkgray";
+    tabButtons[panelIndex].style.color="#da0a7ab";
     tabPanels.forEach(function(node){
         node.style.display="none";
     });
     tabPanels[panelIndex].style.display="block";
     tabPanels[panelIndex].style.backgroundColor=colorCode;
 }
-showPanel(0,'#f44336');
+showPanel(0,'#ffffff');
+
+//tables
+function myFunction() {
+
+  let input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
