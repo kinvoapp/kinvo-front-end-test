4d202e83029ab7662f03db6be15152a0430f9848
Danilo Alves <danilovieiraalves@hotmail.com>
2019-09-23 02:14:08 -0300

    Aba lateral e botões prontos

--- kinvo-teste/src/components/SidebarButton.jsx
+++ kinvo-teste/src/components/SidebarButton.jsx
@@ -1,40 +1,32 @@
 import React, { Component } from "react";
 import logo from "./images/button-4-1.png";
 import styled from "styled-components";
-import { CaretRight } from "styled-icons/boxicons-regular/CaretRight";
 
 // styled-components code
-const RedCaret = styled(CaretRight)`
-  color: red;
-`;
 
-const App = () => <RedCaret />;
 const DIV = styled.div`
   font-family: "helvetica-neue, arial";
   font-size: 14px;
   display: flex;
   align-items: center;
-  justify-content: center;
+  padding: 0 20px;
   p {
     padding: 0 10px;
   }
-  width: 225px;
+  width: 200px;
   height: 70px;
-  background-color: #eef2f4;
+  background-color: ${props => (props.getState ? "#eef2f4" : "#ffffff")};
+  border: 1px solid #eef2f4;
 `;
 
 //--------------------
 
 class SidebarButton extends Component {
-  state = { imgSrc: "./images/button-4-1.png", name: "Sem Nome", active: true };
   render() {
     return (
-      <DIV>
-        <img src={logo} width={40} height={40} />
-        <p>
-          {this.state.name}
-          {App()}
-        </p>
+      <DIV getState={this.props.active}>
+        <img src={this.props.imgSrc} width={40} height={40} />
+        <p>{this.props.name}</p>
       </DIV>
     );
   }
--- kinvo-teste/src/index.js
+++ kinvo-teste/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 //import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import SidebarButton from "./components/SidebarButton";
+import SidebarSetOfButtons from "./components/SidebarSetOfButtons";
 
-ReactDOM.render(<SidebarButton />, document.getElementById("root"));
+ReactDOM.render(<SidebarSetOfButtons />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
