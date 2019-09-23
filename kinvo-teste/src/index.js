7c0e269494b3d64cc6ca749509a63ddbf7cdc4d3
Danilo Alves <danilovieiraalves@hotmail.com>
2019-09-23 02:37:57 -0300

    Header pronto

--- kinvo-teste/src/index.js
+++ kinvo-teste/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 //import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import SidebarSetOfButtons from "./components/SidebarSetOfButtons";
+import Header from "./components/Header";
 
-ReactDOM.render(<SidebarSetOfButtons />, document.getElementById("root"));
+ReactDOM.render(<Header />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
