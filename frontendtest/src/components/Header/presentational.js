ea66292fb54d04355b911e74725107876f54fbdb
Joao Pedro <joaopedronapu@yahoo.com.br>
2019-10-11 10:15:14 -0300

    refactor(/components/Header): Changed the semantic of some tags

--- frontendtest/src/components/Header/presentational.js
+++ frontendtest/src/components/Header/presentational.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { Container } from './styles';
 import logoKinvoPremium from './images/logo.svg';
 
-function Header(){
-  return(
+function Header() {
+  return (
     <Container>
-      <a href="#"><img src={logoKinvoPremium}></img></a>
+      <a href="https://www.kinvo.com.br"><img alt="Logo Premium do Kinvo" src={logoKinvoPremium} /></a>
     </Container>
-  )
+  );
 }
 
 export default Header;
