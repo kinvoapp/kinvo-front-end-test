8cf52a89f7d32357e60d3e00ec0d604d7ff8a8f8
Joao Pedro <joaopedronapu@yahoo.com.br>
2019-10-11 14:34:55 -0300

    refact(components/NavigationTab): The 'presentational.js' and styled-components was implemented on NavigationTab component, respecting the company standard

---
+++ frontendtest/src/components/navigationTab/presentational.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import './styles.css';
+import '../../styles.css';
+
+import walletOverviewIcon from './images/walletOverviewIcon.svg';
+import fgcCoveredIcon from './images/fgcCoveredIcon.svg';
+import myProceedsIcon from './images/myProceedsIcon.svg';
+import myProductsIcon from './images/myProductsIcon.svg';
+import productClassAnalysisIcon from './images/productClassAnalysisIcon.svg';
+import realProftabilityIcon from './images/realProftabilityIcon.svg';
+import riskReturnIcon from './images/riskReturnIcon.svg';
+import walletProjectionIcon from './images/walletProjectionIcon.svg';
+
+import NavigationTabItems from './components/NavigationTabItems';
+import { Container } from './styles';
+
+function NavigationTab() {
+  return (
+
+    <Container>
+
+      <NavigationTabItems
+        iconSource={walletOverviewIcon}
+        label="Resumo da Carteira"
+      />
+
+      <NavigationTabItems
+        iconSource={myProductsIcon}
+        label="Meus Produtos"
+      />
+
+      <NavigationTabItems
+        iconSource={myProceedsIcon}
+        label="Meus Proventos"
+      />
+
+      <NavigationTabItems
+        iconSource={productClassAnalysisIcon}
+        label="Análise por Classe de Produto"
+      />
+
+      <NavigationTabItems
+        iconSource={realProftabilityIcon}
+        label="Rentabilidade Real"
+      />
+
+      <NavigationTabItems
+        iconSource={walletProjectionIcon}
+        label="Projeção da Carteira"
+      />
+
+      <NavigationTabItems
+        iconSource={riskReturnIcon}
+        label="Risco x Retorno"
+      />
+
+      <NavigationTabItems
+        iconSource={fgcCoveredIcon}
+        label="Cobertura do FGC"
+      />
+
+    </Container>
+
+  );
+}
+
+export default NavigationTab;
---
+++ frontendtest/src/components/navigationTab/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.aside`
+
+    max-width: 20%;
+    height: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    flex-grow: 1;
+    position: relative; 
+
+    a{
+      text-decoration: none;
+      color: black;
+    }
+
+`;
