f0e1fa946167240809991044950bb4a7a5509ba6
Danilo Alves <danilovieiraalves@hotmail.com>
2019-10-15 15:24:34 -0300

    refactor: re-organized icon importation and props

--- kinvo-teste/src/components/SideBar/presentational.js
+++ kinvo-teste/src/components/SideBar/presentational.js
@@ -2,48 +2,72 @@ import React from 'react';
 
 import SidebarButton from './components/SidebarButton';
 
-import purpleButton from './images/button-4-1.png';
-import emptyButton from './images/empty-button-1.png';
+import portfolioOverview from './images/resumo-da-carteira.svg';
+import myProducts from './images/meus-produtos.svg';
+import myProceeds from './images/meus-proventos.svg';
+import productAnalysis from './images/analise-produto.svg';
+import realProfitability from './images/rentabilidade-real.svg';
+import portfolioProjection from './images/projecao-carteira.svg';
+import returnRisk from './images/risco-retorno.svg';
+import fgcSecurity from './images/cobertura-fgc.svg';
 import { Container } from './styles';
 
 function SidebarSetOfButtons() {
+  const sidebarContent = [
+    {
+      imageSource: portfolioOverview,
+      name: 'Resumo <br /> Da Carteira',
+      active: false,
+    },
+    {
+      imageSource: myProducts,
+      name: 'Meus <br/> Produtos',
+      active: false,
+    },
+    {
+      imageSource: myProceeds,
+      name: 'Meus <br/> Proventos',
+      active: false,
+    },
+    {
+      imageSource: productAnalysis,
+      name: 'Análise por <br/> Classe de Produto',
+      active: true,
+    },
+    {
+      imageSource: realProfitability,
+      name: 'Rentabilidade <br/> Real',
+      active: false,
+    },
+    {
+      imageSource: portfolioProjection,
+      name: 'Projeção <br/> da Carteira',
+      active: false,
+    },
+    {
+      imageSource: returnRisk,
+      name: 'Risco <br/> x Retorno',
+      active: false,
+    },
+    {
+      imageSource: fgcSecurity,
+      name: 'Cobertura <br/> do FGC',
+      active: false,
+    },
+  ];
+
   return (
     <Container>
-      <SidebarButton
-        imgSrc={emptyButton}
-        name="Resumo da Carteira"
-        active={false}
-      />
-      <SidebarButton
-        imgSrc={emptyButton}
-        name="Meus Produtos"
-        active={false}
-      />
-      <SidebarButton
-        imgSrc={emptyButton}
-        name="Meus Proventos"
-        active={false}
-      />
-      <SidebarButton
-        imgSrc={purpleButton}
-        name="Análise por Classe de Produto"
-        active
-      />
-      <SidebarButton
-        imgSrc={emptyButton}
-        name="Rentabilidade Real"
-        active={false}
-      />
-      <SidebarButton
-        imgSrc={emptyButton}
-        name="Projeção da Carteira"
-        active={false}
-      />
-      <SidebarButton
-        imgSrc={emptyButton}
-        name="Cobertura do FGC"
-        active={false}
-      />
+      {sidebarContent.map((conteudo) => {
+        const { imageSource, name, active } = conteudo;
+        return (
+          <SidebarButton
+            imgSrc={imageSource}
+            name={name}
+            active={active}
+          />
+        );
+      })}
     </Container>
   );
 }
