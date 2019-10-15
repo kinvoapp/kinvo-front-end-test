e06d23ef1f817e360d7c6ca7035acbb90cf80e1e
Danilo Alves <danilovieiraalves@hotmail.com>
2019-10-15 15:20:15 -0300

    feat(FundsSetOfProducts): added Scrollbars

--- kinvo-teste/src/components/BodyContainer/components/WalletContainer/components/ProductsSection/components/FundsSetOfProducts/presentational.js
+++ kinvo-teste/src/components/BodyContainer/components/WalletContainer/components/ProductsSection/components/FundsSetOfProducts/presentational.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import { Container } from './styles';
 import FundsProduct from './components/FundsProduct';
@@ -7,21 +8,23 @@ function FundsSetOfProductsPresentational(props) {
   const { filteredProducts } = props;
 
   return (
-    <Container>
-      {filteredProducts
-        .map((asset) => (
-          <FundsProduct
-            key={Math.random()}
-            productName={asset.productName}
-            equity={asset.equity}
-            amount={asset.amount}
-            averagePrice={asset.averagePrice}
-            lastQuotation={asset.lastQuotation}
-            currentMonthYield={asset.currentMonthYield}
-            lastTwelveMonthYeld={asset.lastTwelveMonthsYeld}
-          />
-        ))}
-    </Container>
+    <Scrollbars autoHide style={{ height: 600 }}>
+      <Container>
+        {filteredProducts
+          .map((asset) => (
+            <FundsProduct
+              key={Math.random()}
+              productName={asset.productName}
+              equity={asset.equity}
+              amount={asset.amount}
+              averagePrice={asset.averagePrice}
+              lastQuotation={asset.lastQuotation}
+              currentMonthYield={asset.currentMonthYield}
+              lastTwelveMonthYeld={asset.lastTwelveMonthsYeld}
+            />
+          ))}
+      </Container>
+    </Scrollbars>
   );
 }
 
--- kinvo-teste/src/components/BodyContainer/components/WalletContainer/components/ProductsSection/components/FundsSetOfProducts/styles.js
+++ kinvo-teste/src/components/BodyContainer/components/WalletContainer/components/ProductsSection/components/FundsSetOfProducts/styles.js
@@ -3,5 +3,5 @@ import styled from 'styled-components';
 export const Container = styled.section`
   display: grid;
   grid-auto-flow: row;
-  grid-gap: 5px;
+  grid-gap: 10px;
 `;
