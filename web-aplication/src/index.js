cdd9805694c22854d978e612ea6b6ab16afbbae8
o4STRO <pedro.cpr@hotmail.com>
2020-02-09 11:28:29 -0300

    adjustSideBars

--- web-aplication/src/index.js
+++ web-aplication/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.unregister();
+
