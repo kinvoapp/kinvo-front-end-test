6733e1ccf61fbcc2a9c45c95786e86c280625612
Adonis Lima <adonis.lima@ufrpe.br>
2020-02-18 14:39:52 -0300

    Header component done
    Signed-off-by: Adonis Lima <adonis.lima@ufrpe.br>

--- kinvo-front-end-test/src/components/layout/Header.js
+++ kinvo-front-end-test/src/components/layout/Header.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './Header.css';
 
+import logo from '../../images/logo.svg';
 
 export class Header extends Component {
   render() {
     return (
       <header>
-        <div className="logo-wrapper">
-          <h1>Header</h1>
+        <div className="branding">
+          <img src={logo}/>
         </div>
       </header>
     )
