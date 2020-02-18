96ee2eafbc4e08f42b3e1f2878ef703514dbe883
Adonis Lima <adonis.lima@ufrpe.br>
2020-02-18 14:41:28 -0300

    Switched App component from function to class
    Signed-off-by: Adonis Lima <adonis.lima@ufrpe.br>

--- kinvo-front-end-test/src/App.js
+++ kinvo-front-end-test/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/layout/Header';
+import Aside from './components/layout/Aside';
+import Section from './components/layout/Section';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      
-    </div>
-  );
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Header/>
+  
+      </div>
+    );
+  }
 }
 
 export default App;
