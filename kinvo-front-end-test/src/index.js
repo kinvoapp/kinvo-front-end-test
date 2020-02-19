528e6226faed62f9cca0200a7bb50b102fc6779f
Adonis Lima <adonis.lima@ufrpe.br>
2020-02-19 10:42:59 -0300

    'Helvetica Neue' font installed
    Signed-off-by: Adonis Lima <adonis.lima@ufrpe.br>

--- kinvo-front-end-test/src/index.js
+++ kinvo-front-end-test/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
+import './fonts/helvetica-neue.ttf'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
