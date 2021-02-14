656bb2ca2eeac362a6f98cf9987a11bf1431cb49
Gabriel GUX <gabrielsrbezerra@gmail.com>
2021-02-14 00:25:01 -0300

    get general data from api

--- app/src/templates/main/fixed_income/index.js
+++ app/src/templates/main/fixed_income/index.js
@@ -1,12 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { LateralNav, TopNav } from '../components'
 import { Container } from './styles'
 import fixedIncomeAPI from '../../../services/fixedIncome'
 
 function FixedIncome() {
 
+    const [portifolioData, setPortifolioData] = useState()
+
+
     useEffect(() => {
-        fixedIncomeAPI.getFixedIncomeData().then((res) => console.log(res))
+        fixedIncomeAPI.getFixedIncomeData().then((res) => {
+            if (res.data.success) {
+                setPortifolioData(res.data.data.snapshotByPortfolio)
+            }
+        })
     }, [])
 
     return (
@@ -16,45 +23,46 @@ function FixedIncome() {
             <div className='content_container'>
                 <h1>Renda Fixa</h1>
 
-                <div className='small_card_container'>
-                    <div className='small_card'>
-                        <div>
-                            <p>Saldo Bruto</p>
-                            <h5>R$ 207.653,10</h5>
+                {portifolioData &&
+                    <div className='small_card_container'>
+                        <div className='small_card'>
+                            <div>
+                                <p>Saldo Bruto</p>
+                                <h5>{Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' }).format(portifolioData.equity)}</h5>
+                            </div>
                         </div>
-                    </div>
-                    <div className='small_card'>
-                        <div>
-                            <p>Valor Aplicado</p>
-                            <h5>R$ 170.025,64</h5>
+                        <div className='small_card'>
+                            <div>
+                                <p>Valor Aplicado</p>
+                                <h5>{Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' }).format(portifolioData.valueApplied)}</h5>
+                            </div>
                         </div>
-                    </div>
-                    <div className='small_card'>
-                        <div>
-                            <p>Resultado</p>
-                            <h5>R$ 37.638,46</h5>
+                        <div className='small_card'>
+                            <div>
+                                <p>Resultado</p>
+                                <h5>{Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' }).format(portifolioData.equityProfit)}</h5>
+                            </div>
                         </div>
-                    </div>
-                    <div className='small_card'>
-                        <div>
-                            <p>Rentabilidade</p>
-                            <h5>25,08%</h5>
+                        <div className='small_card'>
+                            <div>
+                                <p>Rentabilidade</p>
+                                <h5>{Intl.NumberFormat('pt-br').format(portifolioData.percentageProfit)}%</h5>
+                            </div>
                         </div>
-                    </div>
-                    <div className='small_card'>
-                        <div>
-                            <p>CDI</p>
-                            <h5>23,68%</h5>
+                        <div className='small_card'>
+                            <div>
+                                <p>CDI</p>
+                                <h5>{Intl.NumberFormat('pt-br').format(portifolioData.indexerValue)}%</h5>
+                            </div>
                         </div>
-                    </div>
-                    <div className='small_card'>
-                        <div>
-                            <p>% sobre cdi</p>
-                            <h5>320%</h5>
+                        <div className='small_card'>
+                            <div>
+                                <p>% sobre cdi</p>
+                                <h5>{Intl.NumberFormat('pt-br').format(portifolioData.percentageOverIndexer)}%</h5>
+                            </div>
                         </div>
-                    </div>
 
-                </div>
+                    </div>}
 
             </div>
         </Container>
