85fea69f6cb766811276902c9022b1f4be437eda
Gabriel GUX <gabrielsrbezerra@gmail.com>
2021-02-14 16:17:41 -0300

    started adding pagination

--- app/src/templates/main/fixed_income/index.js
+++ app/src/templates/main/fixed_income/index.js
@@ -13,13 +13,22 @@ function FixedIncome() {
 
     useEffect(() => {
         fixedIncomeAPI.getFixedIncomeData().then((res) => {
-
             if (res.data.success) {
                 setPortfolioData(res.data.data.snapshotByPortfolio)
-                setProductData(res.data.data.snapshotByProduct)
+                let auxProductData = res.data.data.snapshotByProduct
+                auxProductData[5].fixedIncome.name = 'CDD Teste de Produto'
+                setProductData(auxProductData)
+                console.log(res.data.data.snapshotByProduct)
             }
         })
     }, [])
+
+    const getPaginatedProductData = (page) => {
+        if(productData){
+            return productData.slice((page - 1) * 5 , (page * 5))
+        }
+        return null
+    }
 
     return (
         <Container>
@@ -28,7 +37,7 @@ function FixedIncome() {
             <div className='content_container'>
                 <h1>Renda Fixa</h1>
                 <PortfolioDataContainer portfolioData={portfolioData}/>
-                <IncomeTable productData={productData}/>
+                <IncomeTable productData={getPaginatedProductData(1)}/>
             </div>
         </Container>
     )
