8bf6758836d5124255fec1d28b26bbcc0db7da98
Bianca Magalhães <bianca@12min.com>
2021-03-16 23:30:47 -0300

    fix disable button

--- kinvo-premium/src/components/SideMenu/index.tsx
+++ kinvo-premium/src/components/SideMenu/index.tsx
@@ -3,24 +3,24 @@ import SectionButton from './sections';
 
 export default function SideMenu() {
   const options = [
-    {title: 'Resumo da carteira', iconSrc: '/images/menuIcon1.svg'},
-    {title: 'Meus produtos', iconSrc: '/images/menuIcon2.svg'},
-    {title: 'Meus proventos', iconSrc: '/images/menuIcon3.svg'},
-    {title: 'Classe de ativo', iconSrc: '/images/menuIcon4.svg', openedOptions:[
+    {title: 'Resumo da carteira', iconSrc: '/images/menuIcon1.svg', disabled: true},
+    {title: 'Meus produtos', iconSrc: '/images/menuIcon2.svg', disabled: true},
+    {title: 'Meus proventos', iconSrc: '/images/menuIcon3.svg', disabled: true},
+    {title: 'Classe de ativo', iconSrc: '/images/menuIcon4.svg', disabled: false, openedOptions:[
       'ação',
       'fundo',
       'fundo imobiliário',
       'fundo renda fixa',
     ]},
-    {title: 'Rentabilidade Real', iconSrc: '/images/menuIcon5.svg'},
-    {title: 'Projeção da carteira', iconSrc: '/images/menuIcon6.svg'},
-    {title: 'Risco x Retorno', iconSrc: '/images/menuIcon7.svg'},
-    {title: 'Cobertura do FGC', iconSrc: '/images/menuIcon8.svg'}
+    {title: 'Rentabilidade Real', iconSrc: '/images/menuIcon5.svg', disabled: true},
+    {title: 'Projeção da carteira', iconSrc: '/images/menuIcon6.svg', disabled: true},
+    {title: 'Risco x Retorno', iconSrc: '/images/menuIcon7.svg', disabled: true},
+    {title: 'Cobertura do FGC', iconSrc: '/images/menuIcon8.svg', disabled: true }
   ]
   return (
     <Container>
       {options.map((item) => { return (<SectionButton
-        disabled={true}
+        disabled={item.disabled}
         title={item.title}
         iconSrc={item.iconSrc}
         openedOptions={item.openedOptions}
