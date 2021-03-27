3365f7d2b7223636cf218709660dd38f7c3ede7b
Iure Guimaraes <iurevguimaraes@gmail.com>
2021-03-27 02:26:43 -0300

    add global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return <h1>App</h1>;
-};
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>App</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    line-height: 100%;
+  }
+  html, body {
+    display: flex;
+    width: 100%;
+    min-height: 100%;
+  }
+  body {
+    background: #EEF2F4;
+    font: 18px 'Montserrat', sans-serif;
+    color: #707B81;
+    display: flex;
+    flex-direction: column;
+  }
+
+  #root {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+  }
+`;
