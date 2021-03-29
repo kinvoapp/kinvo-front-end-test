980acca2e870b061b429167b84f8848e59324e85
tarfao <tarfao@hotmail.com>
2021-03-29 19:33:14 -0300

    corrigindo o get para pesquisa

--- src/componentes/MinhasRendasFixas/UseMinhasRendasFixas.js
+++ src/componentes/MinhasRendasFixas/UseMinhasRendasFixas.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 
-function UseMinhasRendasFixas() {
-    const [arrRendasFixas, setArrRendasFixas] = useState([]);
-    const [busca, setBusca] = useState("");
+function UseMinhasRendasFixas(getDados, arrRendasFixas, setArrRendasFixas, setBusca) {
     const [ordenacao, setOrdenacao] = useState("");
 
     const changeOrdenacao = e => {
@@ -42,8 +40,9 @@ function UseMinhasRendasFixas() {
 
     const changeBusca = async (e) => {
         const { value } = e.target;
+        console.log(value);
         setBusca(value);
-        const { data: { data: { snapshotByProduct } } } = await axios.get(parseInt(process.env.REACT_APP_PAGINATION));
+        const { data: { data: { snapshotByProduct } } } = await axios.get(process.env.REACT_APP_URI);
         const filtro = snapshotByProduct.filter(snapshot => snapshot.fixedIncome.name.toUpperCase().includes(value.toUpperCase()))
         setArrRendasFixas(filtro.slice(0, parseInt(process.env.REACT_APP_PAGINATION))); //exibe somente os 5 iniciais
     }
@@ -61,21 +60,9 @@ function UseMinhasRendasFixas() {
         }
     }
 
-    const getDados = async (num_total_dados) => {
-        const { data: { data: { snapshotByProduct } } } = await axios.get(process.env.REACT_APP_URI);
-        if (busca) { //se tem busca, o getDados só é chamado quando tem scroll ou quando a página é criada
-            const filtro = snapshotByProduct.filter(snapshot => snapshot.fixedIncome.name.toUpperCase().includes(busca.toUpperCase()))
-            const new_data = [...arrRendasFixas, ...filtro.slice(num_total_dados, num_total_dados + parseInt(process.env.REACT_APP_PAGINATION))];
-            setArrRendasFixas(new_data);
-        } else {
-            const new_data = [...arrRendasFixas, ...snapshotByProduct.slice(num_total_dados, num_total_dados + parseInt(process.env.REACT_APP_PAGINATION))];
-            setArrRendasFixas(new_data);
-        }
-    }
-
     return {
-        scrollFunc, arrRendasFixas, getDados, ordenacao,
-        changeBusca, changeOrdenacao, busca,
+        scrollFunc, arrRendasFixas, ordenacao,
+        changeBusca, changeOrdenacao,
     }
 }
 
