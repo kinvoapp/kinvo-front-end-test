01c3d97bc43e9e0b45c24ed3910157a8e3c689b6
tarfao <tarfao@hotmail.com>
2021-03-29 19:56:56 -0300

    exibindo os dados dos valores totais no cabeçalho a partir da requisiçao

--- src/componentes/ValoresTotaisCabecalho/index.jsx
+++ src/componentes/ValoresTotaisCabecalho/index.jsx
@@ -1,49 +1,65 @@
+import { useEffect } from "react";
 import { DadosColoridos, TituloDeDados } from "../utils/styles";
 import { LinhaVertical, SectionValoresTotais, WrapperBlocoDado } from "./styles";
 
-function ValoresTotaisCabecalho() {
+function ValoresTotaisCabecalho({ objValoresTotais }) {
+    const {
+        equity,
+        valueApplied,
+        equityProfit,
+        percentageProfit,
+        indexerValue,
+        percentageOverIndexer
+    } = objValoresTotais || {
+        equity: 0,
+        valueApplied: 0,
+        equityProfit: 0,
+        percentageProfit: 0,
+        indexerValue: 0,
+        percentageOverIndexer: 0,
+    };
     return (
         <SectionValoresTotais>
             <WrapperBlocoDado>
                 <LinhaVertical />
                 <WrapperBlocoDado.TituloValor>
                     <TituloDeDados>SALDO BRUTO</TituloDeDados>
-                    <DadosColoridos color="roxo">{207653.10.toLocaleString("pt-BR", { style: 'currency', currency: 'BRL'})}</DadosColoridos>
+                    <DadosColoridos color="roxo">{equity.toLocaleString("pt-BR", { style: 'currency', currency: 'BRL' })}</DadosColoridos>
                 </WrapperBlocoDado.TituloValor>
             </WrapperBlocoDado>
             <WrapperBlocoDado>
                 <LinhaVertical />
                 <WrapperBlocoDado.TituloValor>
                     <TituloDeDados>VALOR APLICADO</TituloDeDados>
-                    <DadosColoridos color="roxo">{207653.10.toLocaleString("pt-BR", { style: 'currency', currency: 'BRL'})}</DadosColoridos>
+                    <DadosColoridos color="roxo">{valueApplied.toLocaleString("pt-BR", { style: 'currency', currency: 'BRL' })}</DadosColoridos>
                 </WrapperBlocoDado.TituloValor>
             </WrapperBlocoDado>
             <WrapperBlocoDado>
                 <LinhaVertical />
                 <WrapperBlocoDado.TituloValor>
                     <TituloDeDados>RESULTADO</TituloDeDados>
-                    <DadosColoridos color="roxo">{207653.10.toLocaleString("pt-BR", { style: 'currency', currency: 'BRL'})}</DadosColoridos>
+                    <DadosColoridos color="roxo">{equityProfit.toLocaleString("pt-BR", { style: 'currency', currency: 'BRL' })}</DadosColoridos>
                 </WrapperBlocoDado.TituloValor>
             </WrapperBlocoDado>
             <WrapperBlocoDado>
                 <LinhaVertical />
                 <WrapperBlocoDado.TituloValor>
                     <TituloDeDados>RENTABILIDADE</TituloDeDados>
-                    <DadosColoridos color="roxo">{207653.10.toLocaleString("pt-BR", { style: 'currency', currency: 'BRL'})}</DadosColoridos>
+                    <DadosColoridos color="roxo">{percentageProfit.toLocaleString("pt-BR")}%</DadosColoridos>
                 </WrapperBlocoDado.TituloValor>
             </WrapperBlocoDado>
             <WrapperBlocoDado>
                 <LinhaVertical />
                 <WrapperBlocoDado.TituloValor>
                     <TituloDeDados>CDI</TituloDeDados>
-                    <DadosColoridos color="roxo">{207653.10.toLocaleString("pt-BR", { style: 'currency', currency: 'BRL'})}</DadosColoridos>
+                    <DadosColoridos color="roxo">{indexerValue.toLocaleString("pt-BR")}%</DadosColoridos>
                 </WrapperBlocoDado.TituloValor>
             </WrapperBlocoDado>
             <WrapperBlocoDado>
                 <LinhaVertical />
                 <WrapperBlocoDado.TituloValor>
                     <TituloDeDados>% SOBRE CDI</TituloDeDados>
-                    <DadosColoridos color="roxo">{207653.10.toLocaleString("pt-BR", { style: 'currency', currency: 'BRL'})}</DadosColoridos>
+                    <DadosColoridos color="roxo">{percentageOverIndexer.toLocaleString("pt-BR")}%</DadosColoridos>
                 </WrapperBlocoDado.TituloValor>
             </WrapperBlocoDado>
         </SectionValoresTotais>
--- src/pages/RendaFixa/index.jsx
+++ src/pages/RendaFixa/index.jsx
@@ -7,9 +7,10 @@ import ValoresTotaisCabecalho from "../../componentes/ValoresTotaisCabecalho";
 function RendaFixa() {
     const [arrRendasFixas, setArrRendasFixas] = useState([]);
     const [busca, setBusca] = useState("");
+    const [objValoresTotais, setObjValoresTotais] = useState(false)
 
     const getDados = async (num_total_dados) => {
-        const { data: { data: { snapshotByProduct } } } = await axios.get(process.env.REACT_APP_URI);
+        const { data: { data: { snapshotByProduct, snapshotByPortfolio } } } = await axios.get(process.env.REACT_APP_URI);
         if (busca) { //se tem busca, o getDados só é chamado quando tem scroll ou quando a página é criada
             const filtro = snapshotByProduct.filter(snapshot => snapshot.fixedIncome.name.toUpperCase().includes(busca.toUpperCase()))
             const new_data = [...arrRendasFixas, ...filtro.slice(num_total_dados, num_total_dados + parseInt(process.env.REACT_APP_PAGINATION))];
@@ -17,6 +18,10 @@ function RendaFixa() {
         } else {
             const new_data = [...arrRendasFixas, ...snapshotByProduct.slice(num_total_dados, num_total_dados + parseInt(process.env.REACT_APP_PAGINATION))];
             setArrRendasFixas(new_data);
+        }
+
+        if(!objValoresTotais){
+            setObjValoresTotais(snapshotByPortfolio);
         }
     }
 
@@ -27,7 +32,7 @@ function RendaFixa() {
     return (
         <div>
             <TituloPagina>Renda Fixa</TituloPagina>
-            <ValoresTotaisCabecalho />
+            <ValoresTotaisCabecalho objValoresTotais={objValoresTotais}/>
             <MinhasRendasFixas 
             getDados={getDados}
             arrRendasFixas={arrRendasFixas}
