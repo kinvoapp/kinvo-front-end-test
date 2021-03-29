9880ec91bd36a5641c5454347f468e70002c0bf7
Lucas Rezende <arrobagemeioponto@gmail.com>
2021-03-29 09:53:16 -0300

    refactor(header): add dynamic header data
    mocked data yet

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,7 @@
+//--------------------------------------------------------------------< hooks >
+import { useState } from "react";
+//--------------------------------------------------------------------< utils >
+import { formatNumber } from "../../utils/formatNumber";
 //-------------------------------------------------------------------< assets >
 import logo from "../../assets/logo.svg";
 import grossBalanceIcon from "../../assets/gross-balance.svg";
@@ -5,10 +9,16 @@ import appliedValueIcon from "../../assets/applied-value.svg";
 import profitabilityIcon from "../../assets/profitability.svg";
 import arrowDownIcon from "../../assets/arrow-down.svg";
 import menuIcon from "../../assets/menu.svg";
+//--------------------------------------------------------------------< mocks >
+import snapshotByPortfolio from "../../mocks/snapshotByPortfolio.json";
 //-------------------------------------------------------------------< styles >
 import { Container, Info } from "./styles";
+//--------------------------------------------------------------------< types >
+import { Portfolio } from "../../types/Portfolio";
 //===============================================================[ < Header > ]
 export function Header() {
+  //-------------------------------------------------------------< properties >
+  const [portfolio] = useState<Portfolio>(snapshotByPortfolio);
   //-----------------------------------------------------------------< return >
   return (
     <Container>
@@ -20,7 +30,7 @@ export function Header() {
             <img src={grossBalanceIcon} alt="Saldo Bruto" />
           </div>
           <p>SALDO BRUTO</p>
-          <strong>130.521.230,02</strong>
+          <strong>{formatNumber(portfolio.equity)}</strong>
         </Info>
 
         <Info>
@@ -28,7 +38,7 @@ export function Header() {
             <img src={appliedValueIcon} alt="Valor Aplicado" />
           </div>
           <p>VALOR APLICADO</p>
-          <strong>521.230,02</strong>
+          <strong>{formatNumber(portfolio.valueApplied)}</strong>
         </Info>
 
         <Info>
@@ -36,7 +46,7 @@ export function Header() {
             <img src={profitabilityIcon} alt="Rentabilidade" />
           </div>
           <p>RENTABILIDADE</p>
-          <strong>2,34%</strong>
+          <strong>{formatNumber(portfolio.percentageProfit)}%</strong>
         </Info>
 
         <Info focus>
