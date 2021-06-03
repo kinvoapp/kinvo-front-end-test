178c3b17466260df9f7bef2eda3d69e1fc01da43
Gabriel Silva <66268621+eusousalvi@users.noreply.github.com>
2021-06-02 21:39:57 -0300

    .

--- challenge/src/services/api.js
+++ challenge/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios.defaults.baseURL = process.env.REACT_APP_BASE_URL; //I'll deploy on netlify it's why a comment that
 axios.defaults.baseURL =
-  'https://1762dd35-8f53-49fd-8b8e-c8134a283cb4.mock.pstmn.io/getFixedIncomeClassData';
+  'https://60b6ad6f17d1dc0017b882fd.mockapi.io/mock/getFixedIncomeClassData';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   async getFixedIncome() {
