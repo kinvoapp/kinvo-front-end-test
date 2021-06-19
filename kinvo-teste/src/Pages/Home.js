af3e704c7cf0b24d784fef6ffbde51a108913b01
Rodrigo Braga <rodrigo.cbraga@hotmail.com>
2021-06-19 12:48:06 -0300

    Criado pasta do react

---
+++ kinvo-teste/src/Pages/Home.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        aloha
+      </header>
+    </div>
+  );
+}
+
+export default Home;
---
+++ kinvo-teste/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './Pages/Home';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
