e61ec4c0d12d114524175cdbbdb8787c6557d3f4
Rodrigo Braga <rodrigo.cbraga@hotmail.com>
2021-06-21 16:17:24 -0300

    finalizado erro e loading

---
+++ kinvo-teste/src/Components/erro.js
@@ -0,0 +1,14 @@
+import { ImSad } from "react-icons/im";
+
+const Erro = () => {
+
+  return (
+   <section className='erro-container'>
+       <div className='erro-h1-box'>4<ImSad/>4</div>
+       <h2 className='erro-h2'>Opa! Página não encontrada</h2>
+       <p className='erro-p'>Lamentamos, mas a página que você está procurando não existe.</p>
+   </section>
+  )
+}
+
+export default Erro
--- kinvo-teste/src/Components/infos.js
+++ kinvo-teste/src/Components/infos.js
@@ -1,5 +1,6 @@
 import { useGlobalContext } from '../context'
 import RendaFixa from './rendaFixa'
+import Erro from './erro'
 
 const Infos = () => {
     const {isLoading, erroConnection, siteSubTopic} = useGlobalContext()
@@ -7,11 +8,11 @@ const Infos = () => {
   return (
     isLoading
     ?
-    <article className='infos-container'><img src="./imgs/preloader.gif" alt="carregando" /></article>
+    <article className='infos-container'><img src="./imgs/preloader.gif" alt="carregando" className='loading-gif'/></article>
     :
     erroConnection
       ?
-      <article className='infos-container'>Deu ruim...</article>
+      <article className='infos-container'><Erro /></article>
       :
       siteSubTopic === 'Fundo Renda Fixa' && <RendaFixa />
   )
