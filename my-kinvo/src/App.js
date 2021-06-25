aa36fc241e3de454f73f4fd4436f0a3d105a4764
Lucas Neiva <lucassneiva.code@gmail.com>
2021-06-25 09:22:26 -0300

    começando projeto novamente com react, adicionando componente App.js

---
+++ my-kinvo/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './comp/Header';
+import FixedIncomes from './comp/FixedIncomes';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <FixedIncomes />
+    </div>
+  );
+}
+
+export default App;
