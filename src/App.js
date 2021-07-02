c672e3dd0a3b64a72f64a314a1c8ee7a84e7f620
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-07-02 12:58:36 -0300

    Initial commit - initial React setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Kinvo Test App START !!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
