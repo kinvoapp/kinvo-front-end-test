75713f96cb7b121425d455d4f32cb87a5e510c63
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-07-02 14:25:33 -0300

    finish context hook setup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Provider from './context/Provider'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
