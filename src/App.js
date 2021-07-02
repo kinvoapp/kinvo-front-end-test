da742a4bbc047ccdf75527e04088fd6603a7f084
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-07-02 11:40:21 -0300

    Initial commit, creating react project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  const kinvoTest = 'Kinvo Front-end Test';
+  return (
+    <React.Fragment>
+      <h1>{kinvoTest}</h1>
+    </React.Fragment>
+  );
+};
+
+export default App;
