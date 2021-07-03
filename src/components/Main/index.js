dfd3a354e5e17091edfb8dddd59c4ed1e874b555
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-07-03 15:09:45 -0300

    Creating Main (Area) Component

---
+++ src/components/Main/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { Wrapper } from './styles';
+
+export const Main = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
---
+++ src/components/Main/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+  display: flex;
+  height: 100vh;
+  background-color: #eef2f4;
+`;
