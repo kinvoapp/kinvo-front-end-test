9ab3c16fdb7723d65173907d7278cbafca4d9a26
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-07-03 15:07:56 -0300

    Creating global style

---
+++ src/globalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Montserrat', 'Open-Sans', Helvetica, Sans-Serif;
+    box-sizing: border-box;
+  }
+`;
