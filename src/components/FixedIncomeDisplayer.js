bffade185c4dee41c3e4701db61226fb7180e78c
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-07-04 01:38:11 -0300

    finish fixed income search by text

--- src/components/FixedIncomeDisplayer.js
+++ src/components/FixedIncomeDisplayer.js
@@ -1,17 +1,51 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import FixedIncomeCard from './FixedIncomeCard';
 import AppContext from '../context/AppContext';
 import '../components/FixedIncomeDisplayer.css';
 
 function FixedIncomeDisplayer() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
     const { snapshotByProduct, isFetching } = useContext(AppContext);
+    
+    useEffect(() => {
+        setSearchResults(snapshotByProduct);
+    }, [isFetching]);
+    
+    useEffect(() => {
+        const results = snapshotByProduct
+            .filter((product) => {
+                const { fixedIncome: { name } } = product;
+                const lowerCaseProductName = name.toLowerCase();
+                const lowerCaseSearchTerm = searchTerm.toLowerCase().trim();
+                return lowerCaseProductName.includes(lowerCaseSearchTerm);
+            });
+        setSearchResults(results);
+    }, [searchTerm]);
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+    }
+
     const renderFixedIncomes = () => (
-        snapshotByProduct.map((income, index) => (
+        searchResults.map((income, index) => (
             <FixedIncomeCard key={ index } data={ income } />
         ))
     );
+
     return (
         <section className="income-displayer">
+            <div className="income-displayer-header">
+                <p>Minhas Rendas Fixas</p>
+                <input
+                    className="search-input"
+                    type="text"
+                    value={ searchTerm }
+                    placeholder="Busca"
+                    onChange={ (event) => handleChange(event) }
+                />
+
+            </div>
             { isFetching ? <div>Loading...</div> : renderFixedIncomes() }
         </section>
     );
