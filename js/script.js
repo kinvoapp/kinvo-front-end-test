a9426b51649f08728a2b77874d881f19fba32425
GabrielHidro <gabrielhidro.dev@gmail.com>
2021-07-04 01:33:43 -0300

    adiciona conteúdo princípal sem gráficos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const openDrop = document.querySelector('.dropdown-content')
 const drop = document.querySelector('.nodrop-content')
+const itemSelect = document.querySelector('li.dropdown .dropdown-content li:last-child')
 
 function openDropdown(){
   openDrop.classList.toggle('active')
   drop.classList.toggle('selected')
+}
+
+function showContent(){
+  itemSelect.classList.add('selected')
 }
