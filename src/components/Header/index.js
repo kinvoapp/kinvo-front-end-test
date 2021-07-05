6c3111de156129d0de94603e33ebc12953cb35c6
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-07-04 21:11:13 -0300

    Finishing header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,75 @@
-/* eslint-disable import/prefer-default-export */
 import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import { useData } from '../../contexts/DataContext';
 import { ReactComponent as Logo } from '../../assets/logo-xd.svg';
-import { Wrapper } from './styles';
+import { ReactComponent as GrossBalance } from '../../assets/gross-balance.svg';
+import { ReactComponent as ValueApplied } from '../../assets/value-applied.svg';
+import { ReactComponent as Rentability } from '../../assets/rentability-2.svg';
+import { ReactComponent as Arrow } from '../../assets/arrow-2.svg';
+import { ReactComponent as Menu } from '../../assets/menu.svg';
+import { Wrapper, Info, Value } from './styles';
 
 export const Header = () => {
+  const {
+    data: {
+      data: { snapshotByPortfolio },
+    },
+  } = useData();
+
   return (
     <Wrapper>
       <div>
         <Logo />
       </div>
+      <Info>
+        <Value title="Saldo Bruto">
+          <div>
+            <GrossBalance />
+          </div>
+          <h2>
+            {snapshotByPortfolio ? (
+              snapshotByPortfolio.equity.toLocaleString('pt-br')
+            ) : (
+              <Skeleton width={90} />
+            )}
+          </h2>
+        </Value>
+        <Value title="Valor Aplicado">
+          <div>
+            <ValueApplied />
+          </div>
+          <h2>
+            {snapshotByPortfolio ? (
+              snapshotByPortfolio.valueApplied.toLocaleString('pt-br')
+            ) : (
+              <Skeleton width={90} />
+            )}
+          </h2>
+        </Value>
+        <Value title="Rentabilidade">
+          <div>
+            <Rentability />
+          </div>
+          <h2>
+            {snapshotByPortfolio ? (
+              `${snapshotByPortfolio.percentageProfit.toLocaleString('pt-br')}%`
+            ) : (
+              <Skeleton width={100} />
+            )}
+          </h2>
+        </Value>
+        <Value title="Carteira" color="#5D41AC" bold cursorPointer>
+          <div>
+            <Arrow />
+          </div>
+          <h2>Minha Carteira</h2>
+        </Value>
+        <Value color="#5D41AC" cursorPointer>
+          <div>
+            <Menu />
+          </div>
+        </Value>
+      </Info>
     </Wrapper>
   );
 };
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -6,5 +6,50 @@ export const Wrapper = styled.header`
   justify-content: space-between;
   align-items: center;
   background-color: #ffffff;
-  padding: 0 28px;
+  padding: 0 50px 0 28px;
+`;
+
+export const Info = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const Value = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: ${(props) => (props.cursorPointer ? 'pointer' : 'default')};
+
+  & + div {
+    margin-left: 30px;
+  }
+
+  > div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 30px;
+    height: 30px;
+    background-color: ${(props) => (props.color ? props.color : '#DAE0E3')};
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+
+  h2 {
+    font-size: 16px;
+    font-weight: bold;
+    color: #707b81;
+    margin: 0;
+    padding: 0;
+
+    &:before {
+      content: '${(props) => props.title}';
+      display: block;
+      font-size: 8px;
+      text-transform: uppercase;
+      color: #707b81;
+      font-weight: ${(props) => (props.bold ? '600' : '400')};
+    }
+  }
 `;
