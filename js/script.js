731f8a678a0a2bd128d26b0cb00c406b92935087
GabrielHidro <gabrielhidro.dev@gmail.com>
2021-07-05 22:01:10 -0300

    Adiciona favicon e revisão

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const openDrop = document.querySelector('.dropdown-content')
 const drop = document.querySelector('.nodrop-content')
 const itemSelect = document.querySelector('li.dropdown .dropdown-content li:last-child')
+const menuMobile = document.querySelector('#menu-mobile')
 
 function openDropdown(){
   openDrop.classList.toggle('active')
@@ -11,6 +12,9 @@ function showContent(){
   itemSelect.classList.add('selected')
 }
 
+menuMobile.addEventListener('click', () => {
+  document.querySelector('.sidebar').classList.toggle('active')
+})
 
 
 
