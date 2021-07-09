14561318d8858f2811cc1325727bf5022e33d8ae
Lucas Neiva <lucassneiva.code@gmail.com>
2021-07-09 20:50:31 -0300

    adiciona componente para renderização dinâmica do card "TITULO"

---
+++ my-kinvo/src/comp/IncomeTitleCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class IncomeTitleCard extends React.Component {
+  render() {
+    const { income } = this.props;
+    const { fixedIncome: { bondType, name } } = income;
+    return (
+      <div className="income-card" data-testid="income-card">
+        <div className="income-card-body">
+          <h5>TÍTULO</h5>
+          <p className="name">{name}</p>
+          <label htmlFor="bondType"
+        data-testid="classe-label"
+            > CLASSE
+          <p className="bondType">{bondType}</p>
+          </label>
+        </div>
+      </div>
+    );
+  }
+}
+
+IncomeTitleCard.propTypes = {
+  income: PropTypes.shape({
+    name: PropTypes.string,
+    bondType: PropTypes.string,
+  }).isRequired,
+};
+
+export default IncomeTitleCard; 
