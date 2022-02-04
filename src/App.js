1f9052fee88ea87cde9844ad9963d9aba32e84db
Luis Eduardo Santos Costa <luiseduardo70@live.com>
2022-02-04 18:02:53 -0300

    Adição do cabeçalho

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import Header from "./components/Header/Header";
+import SideSection from "./components/SideSection/SideSection";
 
 function App() {
-    return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Header />
+      <main>
+        <SideSection />
+      </main>
+    </div>
+  );
 }
 
 export default App;
