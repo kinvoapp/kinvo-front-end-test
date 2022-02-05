46b0deb97329aafe55ba370e03f1ea5a0924ae4a
Luis Eduardo Santos Costa <luiseduardo70@live.com>
2022-02-04 21:44:57 -0300

    Adição do Header e Main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import "./App.css";
+
 import Header from "./components/Header/Header";
-import SideSection from "./components/SideSection/SideSection";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <main>
-        <SideSection />
-      </main>
+      <Main />
     </div>
   );
 }
