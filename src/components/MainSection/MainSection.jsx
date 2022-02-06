91857f4cb6484976e2f18a28a1cdfb5d8813c6e3
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-02-06 19:25:07 -0300

    Valores passados via API

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,8 +1,23 @@
 import "./MainSection.css";
 import React from "react";
+import MainHeader from "../MainHeader/MainHeader";
+import RendasFixas from "../RendasFixas/RendasFixas";
 
-function MainSection() {
-  return <div className='main-section'></div>;
+import grafico1 from "../../assets/main-content/grafico1.svg";
+
+function MainSection({ rendaFixa, minhasRendasFixas, setSearchText }) {
+  return (
+    <div className='main-section'>
+      <MainHeader rendaFixa={rendaFixa} />
+
+      <img src={grafico1} alt='Gráfico de Rentabilidade dos Títulos' />
+
+      <RendasFixas
+        minhasRendasFixas={minhasRendasFixas}
+        setSearchText={setSearchText}
+      />
+    </div>
+  );
 }
 
 export default MainSection;
