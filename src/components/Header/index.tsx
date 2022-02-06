2d2c53f6936ff5f9a14cf5a5bde386c9fb13a1e3
Murilo Vicente <murilovif17@gmail.com>
2022-02-06 11:49:42 -0300

    adding types

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LogoHeader from '../../assets/LogoHeader.svg'
+
+function Header() {
+    return (
+        <>
+            <div>
+                <img src={LogoHeader} />
+            </div>
+        </>
+    );
+};
+
+export default Header
