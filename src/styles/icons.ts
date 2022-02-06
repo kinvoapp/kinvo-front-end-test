7dc4cafbdfa23891f2b10b60652d221bb96c4600
Thales Rodrigues <thales.brito@hotmail.com>
2022-02-05 22:26:43 -0300

    Add new icons

--- src/styles/icons.ts
+++ src/styles/icons.ts
@@ -1,5 +1,14 @@
-export * as moneyIcon from "../assets/icons/money.svg";
 export * as arrowChevronDown from "../assets/icons/arrow_chevron_down.svg";
+export * as arrowChevronRightDarkIcon from "../assets/icons/arrow_chevron_right_dark.svg";
 export * as arrowUpIcon from "../assets/icons/arrow_up.svg";
 export * as arrowWaveUpRightIcon from "../assets/icons/arrow_wave_up_right.svg";
+export * as chartIcon from "../assets/icons/chart.svg";
+export * as donutGraphIcon from "../assets/icons/donut_graph.svg";
+export * as diamondIcon from "../assets/icons/diamond.svg";
 export * as hamburgerMenuIcon from "../assets/icons/hamburger_menu.svg";
+export * as moneyBagIcon from "../assets/icons/money_bag.svg";
+export * as moneySignIcon from "../assets/icons/money_sign.svg";
+export * as padlockIcon from "../assets/icons/padlock.svg";
+export * as pizzaSliceQuarter from "../assets/icons/pizza_slice_quarter.svg";
+export * as puzzlePieceIcon from "../assets/icons/puzzle_piece.svg";
+export * as spiralIcon from "../assets/icons/spiral.svg";
