d604a68ffbc001f7ce622ff33555c69aece635e4
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-02-06 19:24:45 -0300

    Passagem das referências dos states

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,11 +3,16 @@ import React from "react";
 import SideSection from "../SideSection/SideSection";
 import MainSection from "../MainSection/MainSection";
 
-function Main({ rendaFixa }) {
+function Main({ rendaFixa, minhasRendasFixas, setSearchText }) {
   return (
     <main className='main-content'>
       <SideSection />
-      <MainSection rendaFixa={rendaFixa} />
+
+      <MainSection
+        rendaFixa={rendaFixa}
+        minhasRendasFixas={minhasRendasFixas}
+        setSearchText={setSearchText}
+      />
     </main>
   );
 }
