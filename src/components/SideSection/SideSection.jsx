941d6de845852aae30923a1a07d6375c2dbfd148
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-02-06 21:22:05 -0300

    Adição da subção da SideSection

--- src/components/SideSection/SideSection.jsx
+++ src/components/SideSection/SideSection.jsx
@@ -10,22 +10,50 @@ import produtos from "../../assets/side-content/produtos.svg";
 import projecao from "../../assets/side-content/projecao.svg";
 import resumo from "../../assets/side-content/resumo.svg";
 import risco from "../../assets/side-content/risco.svg";
+import expand_icon from "../../assets/side-content/expand.svg";
 
 //? Componentes
 import SideSectionContent from "../SideSectionContent/SideSectionContent";
 
+const SelectedSection = ({ title, icon }) => {
+  return (
+    <>
+      <div className='select-side-section-content'>
+        <div className='section-icon-area'>
+          <div className='dot'></div>
+        </div>
+
+        <div className='section-title-area'>
+          <h3 className='section-content-title'>{title}</h3>
+        </div>
+
+        <div className='section-expand'>
+          <img src={icon} alt='Expand Icon' />
+        </div>
+      </div>
+    </>
+  );
+};
+
 const SideSection = () => {
   return (
     <div className='side-section'>
-      {/* //TODO Conteiner geral com 100% de largura */}
-      {/* //TODO Ícone SVG */}
-      {/* //TODO Titulo */}
-      {/* //TODO border bottom */}
-      {/* //TODO Ícone SVG para abrir seção */}
       <SideSectionContent iconSvg={resumo} title='Resumo Da Carteira' />
       <SideSectionContent iconSvg={produtos} title='Meus Produtos' />
       <SideSectionContent iconSvg={dinheiro} title='Meus proventos' />
       <SideSectionContent iconSvg={ativos} title='Classes de Ativos' />
+      <SelectedSection iconSvg={"dot"} title='Ação' icon={expand_icon} />
+      <SelectedSection iconSvg={"dot"} title='Fundo' icon={expand_icon} />
+      <SelectedSection
+        iconSvg={"dot"}
+        title='Fundo Imobiliário'
+        icon={expand_icon}
+      />
+      <SelectedSection
+        iconSvg={"dot"}
+        title='Fundo de Renda Fixa'
+        icon={expand_icon}
+      />
       <SideSectionContent iconSvg={diamante} title='Rentabilidade Real' />
       <SideSectionContent iconSvg={projecao} title='Projeção de Carteira' />
       <SideSectionContent iconSvg={risco} title='Risco x Retorno' />
