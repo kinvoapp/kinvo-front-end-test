43309d3276ea1e1915812c7d857bfeed04d3dc3d
pedrobarbosa0 <47837747+pedrobarbosa0@users.noreply.github.com>
2022-02-06 23:03:35 -0300

    feat(icons, db.json, and api.ts): add all icons, update db.json and api.ts

--- kinvo/src/services/api.ts
+++ kinvo/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-	baseURL: process.env.API_URL,
+	baseURL:
+		process.env.API_URL ||
+		"https://60b6ad6f17d1dc0017b882fd.mockapi.io/mock/getFixedIncomeClassData",
 });
 
 export const db = axios.create({
