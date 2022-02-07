4c7022927d64637e941dd31c1b08cfcf75b45291
pedrobarbosa0 <47837747+pedrobarbosa0@users.noreply.github.com>
2022-02-06 23:05:52 -0300

    feat(bond summary): add BondSummary

---
+++ kinvo/src/components/BondSummary/index.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SummaryContainer = styled.div`
+	display: flex;
+	width: inherit;
+	height: fit-content;
+	flex-direction: row;
+	justify-content: center;
+	padding: 20px;
+	gap: 10px;
+`;
---
+++ kinvo/src/components/BondSummary/index.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import { api } from "../../services/api";
+import { SummaryCard } from "../atoms/SummaryCard";
+import * as S from "./index.style";
+
+interface Summary {
+	equity: number;
+	valueApplied: number;
+	equityProfit: number;
+	percentageProfit: number;
+	indexerValue: number;
+	percentageOverIndexer: number;
+}
+
+export const BondSummary = () => {
+	const [summary, setSummary] = useState<Summary | undefined>();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const { data } = await api.get("/");
+			setSummary(data?.data?.snapshotByPortfolio);
+			console.log(summary);
+		};
+
+		fetchData();
+	}, []);
+
+	return (
+		<S.SummaryContainer>
+			<SummaryCard value={summary?.equity} label="SALDO BRUTO" valueType="currency" />
+			<SummaryCard
+				value={summary?.valueApplied}
+				label="VALOR APLICADO"
+				valueType="currency"
+			/>
+			<SummaryCard value={summary?.equityProfit} label="RESULTADO" valueType="currency" />
+			<SummaryCard
+				value={summary?.percentageProfit}
+				label="RENTABILIDADE"
+				valueType="percent"
+			/>
+			<SummaryCard value={summary?.indexerValue} label="CDI" valueType="percent" />
+			<SummaryCard
+				value={summary?.percentageOverIndexer}
+				label="% SOBRE CDI"
+				valueType="percent"
+			/>
+		</S.SummaryContainer>
+	);
+};
