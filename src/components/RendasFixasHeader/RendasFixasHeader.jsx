88dedbca2cbe9a62a25f8481a492baf7631aac7f
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-02-06 21:21:46 -0300

    Adição da função setOrderType

--- src/components/RendasFixasHeader/RendasFixasHeader.jsx
+++ src/components/RendasFixasHeader/RendasFixasHeader.jsx
@@ -1,19 +1,23 @@
 import "./RendasFixasHeader.css";
-import React, { useState } from "react";
-
 import lupa from "../../assets/main-content/lupa.svg";
 
-function RendasFixasHeader({ setSearchText }) {
+function RendasFixasHeader({ setSearchText, setOrderType }) {
   return (
     <>
       <div className='rendas-fixas-header'>
         <h3>Minhas Rendas Fixas</h3>
 
         <div className='orderer-area'>
-          <select name='' id='select'>
+          <select
+            name=''
+            id='select'
+            onChange={(e) => setOrderType(e.target.value)}
+          >
             <option value='' selected disabled hidden>
               Ordenar por
             </option>
+            <option value='Valor investido'>Valor investido</option>
+            <option value='Saldo bruto'>Saldo bruto</option>
           </select>
 
           <div className='search-area'>
