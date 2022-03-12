85fb50d32cf206b3f5c5e2debac319b7b37af633
Francismar Condor Palma <francismar.condor@outlook.com>
2022-03-12 14:50:18 -0300

    App inicial (draft básico da página)

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div>
+			<header>
+				<h1>KinvoPremium</h1>
+				<article>
+					<h2>Resumo</h2>
+					<article>
+						<h3>Saldo bruto</h3>
+						<p>R$ 207,00</p>
+					</article>
+					<article>
+						<h3>Valor Aplicado</h3>
+						<p>R$ 207,00</p>
+					</article>
+					<article>
+						<h3>Rentabilidade</h3>
+						<p>R$ 207,00</p>
+					</article>
+				</article>
+				<div>
+					<button>Minha carteira</button>
+					<ul>
+						<li>Opção</li>
+					</ul>
+				</div>
+				<div>
+					<button>Icone do whopper</button>
+					<ul>
+						<li>Opção</li>
+					</ul>
+				</div>
+			</header>
+			<div>
+				<nav>
+					<ul>
+						<li>resumo da carteira</li>
+						<li>meus produtos</li>
+					</ul>
+				</nav>
+				<main>
+					<h2>Renda Fixa</h2>
+					<article>
+						<h3>Resumo</h3>
+						<article>
+							<h4>Saldo bruto</h4>
+							<p>R$ 207,00</p>
+						</article>
+						<article>
+							<h4>Valor Aplicado</h4>
+							<p>R$ 207,00</p>
+						</article>
+						<article>
+							<h4>Resultado</h4>
+							<p>R$ 207,00</p>
+						</article>
+					</article>
+					<article>
+						<h3>Rentabilidade dos títulos</h3>
+					</article>
+					<article>
+						<h3>Minhas rendas fixas</h3>
+					</article>
+					<article>
+						<h3>Divisão de carteira por tipos</h3>
+					</article>
+					<article>
+						<h3>Divisão de carteira por título</h3>
+					</article>
+				</main>
+			</div>
+		</div>
+	);
+}
+
+export default App;
