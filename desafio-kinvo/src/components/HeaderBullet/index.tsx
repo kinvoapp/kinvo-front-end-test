da8c7a670bbee70fe2d493785bbb384fa85dd573
Lucas Almeida Costa <lucasdevba@gmail.com>
2022-04-30 17:10:31 -0300

    feat(HeaderBullet): :sparkles: Create

---
+++ desafio-kinvo/src/components/HeaderBullet/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type BulletProps = {
+  thumbnail: string;
+  title: string;
+  value: string;
+};
+
+function HeaderBullet(props: BulletProps) {
+  const { thumbnail, title, value } = props;
+  return (
+    <div>
+      <img src={thumbnail} alt="" />
+      <h3> {title} </h3>
+      <p>{value}</p>
+    </div>
+  );
+}
+
+export default HeaderBullet;
