feb14fbccdff8b7987415a2ca2080696faa96e39
Lucas Almeida Costa <lucasdevba@gmail.com>
2022-05-01 20:03:24 -0300

    feat: :sparkles: Create services to request api

---
+++ desafio-kinvo/src/services/api.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch';
+
+async function FetchIncomeData() {
+  try {
+    const response = await fetch('https://60b6ad6f17d1dc0017b882fd.mockapi.io/mock/getFixedIncomeClassData');
+    return await response.json();
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export default FetchIncomeData;
