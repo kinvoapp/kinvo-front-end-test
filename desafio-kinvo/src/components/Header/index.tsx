25cfd64e173fdf9e82b10b913cf43bf44236f836
Lucas Almeida Costa <lucasdevba@gmail.com>
2022-05-01 17:07:46 -0300

    refactor(Header): :recycle: Componentize Header

--- desafio-kinvo/src/components/Header/index.tsx
+++ desafio-kinvo/src/components/Header/index.tsx
@@ -1,47 +1,21 @@
-import React, { useContext } from 'react';
-import HeaderBullet from '../HeaderBullet';
+import React from 'react';
+import WalletValues from '../WalletValues';
 import WalletInfo from '../WalletInfo';
 import BurguerMenu from '../BurguerMenu';
-import context from '../../contexts/context';
 
-import groosBalanceTmb from '../../assets/header/gross-balance.svg';
-import amountAppliedTmb from '../../assets/header/value-aplication.svg';
-import profitRateTmb from '../../assets/header/profitability.svg';
 import logo from '../../assets/header/logo.svg';
 
 function Header() {
-  const {
-    grossBalance,
-    amountApplied,
-    profitRate,
-  } = useContext(context);
-
-  const bullets = [
-    {
-      thumbnail: groosBalanceTmb,
-      title: 'Saldo Bruto',
-      value: grossBalance,
-    },
-    {
-      thumbnail: amountAppliedTmb,
-      title: 'Valor Aplicado',
-      value: amountApplied,
-    },
-    {
-      thumbnail: profitRateTmb,
-      title: 'Rentabilidade',
-      value: profitRate,
-    },
-  ];
-
   return (
     <header>
-      <img src={logo} alt="" />
-      {bullets.map(({ thumbnail, title, value }) => (
-        <HeaderBullet {...{ thumbnail, title, value }} />
-      ))}
-      <WalletInfo />
-      <BurguerMenu />
+      <img className="logo" src={logo} alt="" />
+      <WalletValues />
+      <div>
+        <div className="Wallet">
+          <WalletInfo />
+        </div>
+        <BurguerMenu />
+      </div>
     </header>
   );
 }
---
+++ desafio-kinvo/src/components/WalletValues/index.tsx
@@ -0,0 +1,45 @@
+import React, { useContext } from 'react';
+import context from '../../contexts/context';
+import HeaderBullet from '../HeaderBullet';
+
+import groosBalanceTmb from '../../assets/header/gross-balance.svg';
+import amountAppliedTmb from '../../assets/header/value-aplication.svg';
+import profitRateTmb from '../../assets/header/profitability.svg';
+
+function WalletValues() {
+  const {
+    grossBalance,
+    amountApplied,
+    profitRate,
+  } = useContext(context);
+
+  const bullets = [
+    {
+      thumbnail: groosBalanceTmb,
+      title: 'Saldo Bruto',
+      value: grossBalance,
+    },
+    {
+      thumbnail: amountAppliedTmb,
+      title: 'Valor Aplicado',
+      value: amountApplied,
+    },
+    {
+      thumbnail: profitRateTmb,
+      title: 'Rentabilidade',
+      value: profitRate,
+    },
+  ];
+
+  return (
+    <Container>
+      <div>
+        {bullets.map(({ thumbnail, title, value }) => (
+          <HeaderBullet key={title} {...{ thumbnail, title, value }} />
+        ))}
+      </div>
+    </Container>
+  );
+}
+
+export default WalletValues;
