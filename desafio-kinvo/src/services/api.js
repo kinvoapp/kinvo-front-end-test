2eb89da2f156de490b7c618196289800e3feb11f
Lucas Almeida Costa <lucasdevba@gmail.com>
2022-05-01 23:38:55 -0300

    refactor: :recycle: Import fix

--- desafio-kinvo/src/services/api.js
+++ desafio-kinvo/src/services/api.js
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch';
-
 async function FetchIncomeData() {
   try {
     const response = await fetch('https://60b6ad6f17d1dc0017b882fd.mockapi.io/mock/getFixedIncomeClassData');
