e71f36fce8e9b34e52590a635a7991fcf1956078
Lucas Almeida Costa <lucasdevba@gmail.com>
2022-05-01 23:51:37 -0300

    feat: :sparkles: Add search functionality

--- desafio-kinvo/src/components/AssetsTableHeader/index.tsx
+++ desafio-kinvo/src/components/AssetsTableHeader/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import fixedIncomeContext from '../../contexts/fixedIncome';
 
 function AssetsTableHeader() {
+  const { filterAssetsByName } = useContext(fixedIncomeContext);
   return (
     <div>
       <h2>Minhas Renda Fixas</h2>
       <select aria-label="search-field">
         <option value="sesx">Ordenar por</option>
       </select>
-      <input type="text" />
+      <input type="text" onChange={({ target: { value } }) => filterAssetsByName(value)} />
     </div>
   );
 }
