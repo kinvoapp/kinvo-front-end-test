644f94c3c17ed2a60ef54b44916a6a356a0a5b5d
Lucas Almeida Costa <lucasdevba@gmail.com>
2022-05-01 23:49:47 -0300

    feat: :sparkles: Add styles

--- desafio-kinvo/src/App.jsx
+++ desafio-kinvo/src/App.jsx
@@ -3,8 +3,6 @@ import Router from './Router';
 import Provider from './contexts/Provider';
 import Header from './components/Header';
 
-// import { Container } from './styles';
-
 function App() {
   return (
     <Provider>
