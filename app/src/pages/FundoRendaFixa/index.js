61334ab170b51787c2ffeb8ef12b6ee73cc0b9b6
Joelson Fernandes <fernandesjoelson97@gmail.com>
2022-10-19 10:27:53 -0300

    added cards in fixed income header

--- app/src/pages/FundoRendaFixa/index.js
+++ app/src/pages/FundoRendaFixa/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import HeaderCards from './components/headerCards';
+
 export default function RendaFixa() {
   return (
     <Container>
       <h1>Renda Fixa</h1>
+      <Header>
+        <HeaderCards />
+      </Header>
     </Container>
   );
 }
@@ -12,4 +17,13 @@ export default function RendaFixa() {
 const Container = styled.main`
   width: 100vw;
   padding: 20px 15px;
+
+  h1 {
+    color: #4c309b;
+  }
+`;
+
+const Header = styled.div`
+  display: flex;
+  margin: 20px 0;
 `;
