9da13cebe5d201f5aae76cdfe51a67adfc6cf9e4
Gabriel Aquino <gabriel.aquino@outlook.com>
2023-04-17 13:20:11 -0300

    Update aside-component.vue with new styles and icons for better user experience

--- my-app/components/aside-component.vue
+++ my-app/components/aside-component.vue
@@ -4,8 +4,8 @@
       <div class="flex h-screen flex-col justify-between bg-white">
         <div class="">
           <nav aria-label="Main Nav" class="mt-6 flex flex-col space-y-1 cursor-pointer mx-2">
-            <a class="flex items-center gap-2 rounded-lg bg-gray-100 px-4 py-2 text-gray-700">
-              <Icon name="carbon:3d-cursor" />
+            <a class="flex items-center gap-2 rounded-lg h-16 bg-gray-100 px-4 py-2 text-gray-700">
+              <Icon name="carbon:3d-cursor" size="2em" class="bg-gray-200 p-1 focus:bg-[#5d41ac] rounded-full" />
               <span class="text-sm font-medium">
                 {{ $t('wallet_resume') }}
               </span>
@@ -14,7 +14,7 @@
             <details class="group [&_summary::-webkit-details-marker]:hidden ">
               <summary class="flex cursor-pointer items-center  justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
                 <div class="flex items-center gap-2">
-                  <Icon name="carbon:wallet" class="bg-gray-100 rounded-full" />
+                  <Icon name="carbon:wallet" size="2em" class="bg-gray-200 p-1 focus:bg-[#5d41ac] rounded-full" />
                   <span class="text-sm font-medium">
                     Resumo Da Carteira
                   </span>
@@ -26,25 +26,25 @@
               </summary>
 
               <nav aria-label="Teams Nav" class="flex flex-col px-4 bg-slate-100 rounded">
-                <a class="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-slate-300 hover:text-gray-700">
+                <a class="flex items-center gap-2 rounded-lg h-16 px-4 py-2 text-gray-500 hover:bg-slate-300 hover:text-gray-700">
                   <Icon name="carbon:dot-mark" />
                   <span class="text-sm font-medium cursor-pointer">
                     {{ $t('action') }}
                   </span>
                 </a>
-                <a class="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-300 hover:text-gray-700">
+                <a class="flex items-center gap-2 rounded-lg h-16 px-4 py-2 text-gray-500 hover:bg-gray-300 hover:text-gray-700">
                   <Icon name="carbon:dot-mark" />
                   <span class="text-sm font-medium cursor-pointer">
                     {{ $t('fund') }}
                   </span>
                 </a>
-                <a class="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-300 hover:text-gray-700">
+                <a class="flex items-center gap-2 rounded-lg h-16 px-4 py-2 text-gray-500 hover:bg-gray-300 hover:text-gray-700">
                   <Icon name="carbon:dot-mark" />
                   <span class="text-sm font-medium cursor-pointer">
                     {{ $t('reits') }}
                   </span>
                 </a>
-                <a class="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-300 hover:text-gray-700">
+                <a class="flex items-center gap-2 rounded-lg h-16 px-4 py-2 text-gray-500 hover:bg-gray-300 hover:text-gray-700">
                   <Icon name="carbon:dot-mark" />
                   <span class="text-sm font-medium cursor-pointer">
                     {{ $t('fund') }}
@@ -53,31 +53,28 @@
               </nav>
             </details>
 
-            <a class="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-              <Icon name="material-symbols:diamond-outline-rounded" class="bg-gray-100 rounded-full" />
+            <a class="flex items-center gap-2 rounded-lg px-4 py-2 h-16 text-gray-500 hover:text-gray-700">
+              <Icon name="material-symbols:diamond-outline-rounded" size="2em" class="bg-gray-200 p-1 focus:bg-[#5d41ac] rounded-full" />
               <span class="text-sm font-medium">
                 {{ $t('real_rentability') }}
               </span>
             </a>
 
-            <a class="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-              <Icon name="material-symbols:chart-data-outline-rounded" class="bg-gray-100 rounded-full" />
-
+            <a class="flex items-center gap-2 rounded-lg px-4 py-2 h-16 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+              <Icon name="material-symbols:chart-data-outline-rounded" size="2em" class="bg-gray-200 p-1 focus:bg-[#5d41ac] rounded-full" />
               <span class="text-sm font-medium">
                 {{ $t('carrer_projection') }}
               </span>
             </a>
 
-            <a class="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-              <Icon name="teenyicons:lifebuoy-outline" class="bg-gray-100 rounded-full" />
-
+            <a class="flex items-center gap-2 rounded-lg px-4 py-2 h-16 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+              <Icon name="teenyicons:lifebuoy-outline" size="2em" class="bg-gray-200 p-1 focus:bg-[#5d41ac] rounded-full" />
               <span class="text-sm font-medium">
                 {{ $t('return_risk') }}
               </span>
             </a>
-            <a class="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-              <Icon name="material-symbols:lock-outline" class="bg-gray-100 rounded-full" />
-
+            <a class="flex items-center gap-2 rounded-lg px-4 py-2 h-16 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+              <Icon name="material-symbols:lock-outline" size="2em" class="bg-gray-200 p-1 focus:bg-[#5d41ac] rounded-full" />
               <span class="text-sm font-medium">
                 {{ $t('FGC_protection') }}
               </span>
